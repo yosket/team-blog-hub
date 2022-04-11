9df4c9e28d9d9e50679c2ee89a626349b2b37620
Yosuke Tanaka <yosket87@gmail.com>
2022-04-12 00:57:32 +0900

    Update

--- site.config.ts
+++ site.config.ts
@@ -1,25 +1,25 @@
 export const config = {
   siteMeta: {
-    title: "Team Blog Hub",
-    teamName: "catnose Inc.",
-    description: "RSS based blog starter kit for teams.",
+    title: "DJT Blog Hub",
+    teamName: "double jump.tokyo",
+    description: "",
   },
   siteRoot:
     process.env.NODE_ENV === "production"
       ? "https://team-blog-hub.vercel.app"
       : "http://localhost:3000",
   headerLinks: [
-    {
-      title: "About",
-      href: "/about",
-    },
+    // {
+    //   title: "About",
+    //   href: "/about",
+    // },
     {
       title: "Company",
-      href: "https://zenn.dev/catnose99/articles/cb72a73368a547756862",
+      href: "https://www.doublejump.tokyo/",
     },
-    {
-      title: "GitHub",
-      href: "https://github.com/catnose99/team-blog-hub",
-    },
+    // {
+    //   title: "GitHub",
+    //   href: "https://github.com/catnose99/team-blog-hub",
+    // },
   ],
 };
